ac426629c412c3af5a7d83630770f04820c15998
gusadi <gusadi872@gmail.com>
2020-07-15 17:00:39 +0800

    Initial Commit

---
+++ Game Sub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game Sub
+//
+//  Created by Agus Adi Pranata on 15/07/20.
+//  Copyright © 2020 gusadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
