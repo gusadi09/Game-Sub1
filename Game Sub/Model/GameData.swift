fe2a461e812e64f062b7e1a5b6f654a4b03e97c0
gusadi <gusadi872@gmail.com>
2020-07-16 23:32:58 +0800

    Add New File

---
+++ Game Sub/Model/GameData.swift
@@ -0,0 +1,9 @@
+//
+//  GameData.swift
+//  Game Sub
+//
+//  Created by Agus Adi Pranata on 15/07/20.
+//  Copyright © 2020 gusadi. All rights reserved.
+//
+
+import Foundation
---
+++ Game Sub/Model/GameManager.swift
@@ -0,0 +1,9 @@
+//
+//  GameManager.swift
+//  Game Sub
+//
+//  Created by Agus Adi Pranata on 15/07/20.
+//  Copyright © 2020 gusadi. All rights reserved.
+//
+
+import Foundation
